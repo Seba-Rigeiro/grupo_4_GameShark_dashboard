5b06dcbc0dd09b2c9b2052b69c50c164e6b80a16
FelEstrela <festrela@fvet.uba.ar>
2021-05-04 00:02:51 -0300

    corregida ruta del logo en Brand.js

--- src/components/Sidebar/Brand.js
+++ src/components/Sidebar/Brand.js
@@ -4,7 +4,7 @@ function Brand(){
     return(
         <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
             <div className="sidebar-brand-icon">
-                <img className="w-100" src="./src/assets/images/logo-prueba.png" alt="Game Shark"></img>
+                <img className="w-100" src="images/logo-prueba.png" alt="Game Shark"></img>
             </div>
         </a>
     )
